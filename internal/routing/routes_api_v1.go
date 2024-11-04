03f463426aa5e6d97e5cd6e3126a7c39853e9723
Mikhail <salazar290720035017@gmail.com>
2024-11-04 09:47:20 +0700

    add register route

--- internal/routing/routes_api_v1.go
+++ internal/routing/routes_api_v1.go
@@ -1,12 +1,12 @@
 package routing
 
 import (
-	"database/sql"
 	"github.com/gin-gonic/gin"
-	"shortener-auth/internal/common"
 	appactions "shortener-auth/internal/common/http_actions"
 )
 
-func Register(engine *gin.Engine, db *sql.DB, ctx *common.ApplicationContext) {
+func Register(engine *gin.Engine) {
 	engine.GET("/healthcheck", appactions.HandleHealth)
+	engine.GET("/register", appactions.HandleRegister)
+
 }
