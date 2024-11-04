e7b15b6b038517e2188d8222ab42377cab3a761f
Mikhail <salazar290720035017@gmail.com>
2024-11-04 09:54:55 +0700

    add register.go

---
+++ internal/common/http_actions/register.go
@@ -0,0 +1,11 @@
+package http_actions
+
+import "github.com/gin-gonic/gin"
+
+func HandleRegister(context *gin.Context) {
+	// достать пароль логин
+	// содать в бд
+	// обработать unique error
+	// сгенерить жвт
+	// вернуть жвт
+}
