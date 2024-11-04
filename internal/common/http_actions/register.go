0f976859b8fb27795bb2fce3a46e8a290fe612ab
Mikhail <salazar290720035017@gmail.com>
2024-11-04 11:12:29 +0700

    fix register action

--- internal/common/http_actions/register.go
+++ internal/common/http_actions/register.go
@@ -1,11 +1,36 @@
 package http_actions
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"shortener-auth/internal/common/service"
+)
 
-func HandleRegister(context *gin.Context) {
-	// достать пароль логин
-	// содать в бд
-	// обработать unique error
-	// сгенерить жвт
-	// вернуть жвт
+type RegisterAction struct {
+	registerService *service.RegisterService
+}
+
+type RegisterRequest struct {
+	Login    string `json:"login" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (a RegisterAction) HandleRegister(context *gin.Context) {
+	var registerRequest RegisterRequest
+
+	if err := context.ShouldBind(&registerRequest); err != nil {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := a.registerService.Register(registerRequest.Login, registerRequest.Password)
+
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(400, gin.H{"error": "error while saving shorten url"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"token": token})
 }
